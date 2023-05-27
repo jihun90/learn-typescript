9c4e9b81f48f6ae071938e9c8c14eb38456f549b
jihun90 <jihun90.kim@icloud.com>
2023-05-28 08:46:42 +0900

    js에서 type을 선언

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,17 +1,34 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
 
 // user data
 var user = {};
 
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {string} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response.address;
+});
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
       user = response.data;
       username.innerText = user.name;
