a2e2cb599ba50295b0b2b96489814857271639d7
jihun90 <jihun90.kim@icloud.com>
2023-05-28 09:19:47 +0900

    js도구를 이용하여 타입추론하는 법

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,6 +1,18 @@
-function sumUsingJS(a, b) {
+// function sumUsingJS(a, b) {
+//   return a + b;
+// }
+
+// var resultUsingJS = sum(10, 20);
+// resultUsingJS.toLocaleString(); //js 자동완성 안됨.. ㅠㅠ
+
+// @ts-check
+
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sum(a, b) {
   return a + b;
 }
 
-var resultUsingJS = sum(10, 20);
-resultUsingJS.toLocaleString(); //js 자동완성 안됨.. ㅠㅠ
+sum(10, "20");
