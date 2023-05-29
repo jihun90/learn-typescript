8e3f1f2480caf3093866f6172b5eb634ef607aa0
jihun90 <jihun90.kim@icloud.com>
2023-05-29 13:50:33 +0900

    ts type 정리

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,27 @@
+//JS
+// var str = "hello";
+
+// TS
+let str: string = "hello";
+let num: number = 10;
+
+let arr: Array<number> = [1, 2, 3];
+let items: number[] = [1, 2, 3];
+let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+
+//튜플
+let addr: [string, number] = ["gangnam", 10];
+
+//객체
+let obj: object = {};
+// let person: object = {
+//   name: "kim",
+//   age: 100,
+// };
+let person: {
+  name: string;
+  age: number;
+} = { name: "kim", age: 100 };
+
+//진위값
+let show: Boolean = true;
