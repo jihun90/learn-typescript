874cdf0777a654b9050576eff4f65c86f2e6a6c2
jihun90 <jihun90.kim@icloud.com>
2023-05-29 15:43:08 +0900

    함수 만들기

---
+++ class-note/2_functions.ts
@@ -0,0 +1,14 @@
+function sum1(a: number, b: number): number {
+  return a + b;
+}
+sum1(10, 20);
+//sum(10, 20, 30, 40) //JS는 추가 매개변수가 가능하지만.. TS는 이것을 막아줌!
+
+//function Optional Parameter
+function log(a: string, b?: string): string {
+  let log: string = a + (b ? b : "");
+  return log;
+}
+
+log("hello world");
+log("hello ts", "test");
