dfa6b6785aaca226aac530411190f5e359a3ec85
jihun90 <jihun90.kim@icloud.com>
2023-05-31 06:11:07 +0900

    type-aliases

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,22 @@
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+
+type Person = {
+    name: string;
+    age: number;
+}
+
+var Lee: Person = {
+    name: '이',
+    age: 100
+}
+
+type MyString = string;
+var myStr: MyString = 'hello';
+
+// type Todo = { id: string; title: string; done: boolean };
+// function getTodo(todo: Todo) {
+
+// }
